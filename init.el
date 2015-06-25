b2d1c0eb77a512eecc71ed35f24fc1038b1e13b1
Igor Calabria <igor.calabria@gmail.com>
2015-06-25 17:13:14 -0300

    utilizes use-package

--- init.el
+++ init.el
@@ -1,32 +1,71 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+
 ;; packages
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
-                        ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")))
 (package-initialize)
 
-(defun require-package (package)
-  (setq-default highlight-tabs t)
-  "Install given PACKAGE."
-  (unless (package-installed-p package)
-    (unless (assoc package package-archive-contents)
-      (package-refresh-contents))
-    (package-install package)))
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(require-package 'helm-ag)
-(require-package 'evil)
-(require-package 'helm)
-(require-package 'evil-leader)
-(require-package 'auto-complete)
-(require-package 'projectile)
-(require-package 'coffee-mode)
-(setq helm-projectile-fuzzy-match t)
-(require-package 'helm-projectile) 
-(require-package 'yasnippet)
-(require-package 'color-theme-solarized)
+(require 'use-package)
 
+(use-package coffe-mode)
+(use-package evil)
+(use-package helm-ag)
+
+(use-package helm
+  :ensure t
+  :diminish helm-mode
+  :config
+  (progn
+    (require 'helm-config)
+    (setq helm-recentf-fuzzy-match t)
+    (setq helm-buffers-fuzzy-matching t)
+    (setq helm-M-x-fuzzy-match t)
+    (setq helm-semantic-fuzzy-match t)
+    (setq helm-apropos-fuzzy-match t)
+    (setq helm-lisp-fuzzy-completion t)
+    (define-key helm-map (kbd "C-z") 'helm-select-action)
+    (define-key helm-map (kbd "C-h") 'my-helm-split)
+    (define-key helm-map (kbd "C-v") 'my-helm-vsplit)
+    (define-key helm-map (kbd "C-j") 'helm-next-line)
+    (define-key helm-map (kbd "C-k") 'helm-previous-line))
+  )
+
+(use-package projectile
+  :ensure t
+  :init
+  (setq projectile-completion-system 'helm)
+  :config
+  (progn
+    (projectile-global-mode))
+  )
+
+(use-package helm-projectile
+  :ensure t
+  :init
+  (setq helm-projectile-fuzzy-match t)
+  :config
+  (progn
+    (helm-projectile-on)
+    )
+  )
+(use-package auto-complete
+  :ensure t
+  :config
+  (progn
+    (ac-config-default)
+    (ac-set-trigger-key "TAB")
+    (ac-set-trigger-key "<tab>")
+    )
+  )
 ;;General config
-(projectile-global-mode)
 (scroll-bar-mode -1)
 (setq ring-bell-function 'ignore)
 (setq-default indent-tabs-mode nil)
@@ -44,13 +83,8 @@
 
 ;; Yas
 ;; Autocomplete
-(ac-config-default)
-(ac-set-trigger-key "TAB")
-(ac-set-trigger-key "<tab>")
 
 ;; Projectile
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
 
 ;; HELM
 (defun my-helm-split ()
@@ -67,43 +101,38 @@
                                      (split-window-horizontally)
                                      (find-file file)
                                      ))))
-(require 'helm-config)
-(setq helm-recentf-fuzzy-match t)
-(setq helm-buffers-fuzzy-matching t)
-(setq helm-M-x-fuzzy-match t)
-(setq helm-semantic-fuzzy-match t)
-(setq helm-apropos-fuzzy-match t)
-(setq helm-lisp-fuzzy-completion t)
-(define-key helm-map (kbd "C-z") 'helm-select-action)
-(define-key helm-map (kbd "C-h") 'my-helm-split)
-(define-key helm-map (kbd "C-v") 'my-helm-vsplit)
-(define-key helm-map (kbd "C-j") 'helm-next-line)
-(define-key helm-map (kbd "C-k") 'helm-previous-line)
+(use-package evil
+  :init
+  (progn
+    (use-package evil-leader
+      :init (global-evil-leader-mode)
+      :config
+      (progn
+        (evil-leader/set-leader "<SPC>")
+        (evil-leader/set-key ":" 'helm-M-x)
+        (evil-leader/set-key
+          "f f" 'helm-find-files
+          "p f" 'helm-projectile-find-file
+          "p p" 'projectile-switch-project
+          "r" 'helm-recentf
+          "b b" 'helm-buffers-list
+          "b n" 'evil-next-buffer
+          "b p" 'evil-prev-buffer
+          "w v" 'split-window-horizontally
+          "w s" 'split-window-vertically
+          )
+        )
+      )
+    (evil-mode 1))
+  :config
+  (progn
+    (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+    (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
 
-;; Needs to be on bottom
-(global-evil-leader-mode)
-(evil-leader/set-leader "<SPC>")
-(evil-leader/set-key ":" 'helm-M-x)
-(evil-leader/set-key
-  "f f" 'helm-find-files
-  "p f" 'helm-projectile-find-file
-  "p p" 'projectile-switch-project
-  "r" 'helm-recentf
-  "b b" 'helm-buffers-list
-  "b n" 'evil-next-buffer
-  "b p" 'evil-prev-buffer
-  "w v" 'split-window-horizontally
-  "w s" 'split-window-vertically
+    ;; Changing windows
+    (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+    (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+    (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+    (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+    )
   )
-(evil-define-key 'normal org-mode-map
-  (kbd "M-j") 'org-metadown
-  (kbd "M-k") 'org-metaup
-  )
-(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
-(define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
-;; Changing windows
-(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-(evil-mode 1)
