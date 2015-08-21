a9cdd968dbe0dc2b5917514270b09d175e4079f4
Igor Calabria <igor.calabria@gmail.com>
2015-08-21 16:22:33 -0300

    Adds hbs to web-mode hook

--- init.el
+++ init.el
@@ -91,6 +91,7 @@
     (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 
     (setq web-mode-markup-indent-offset 2)
     )
