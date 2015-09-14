ccab16bd479e7b462de76cae7a4fd1024eb989cf
Igor Calabria <igor.calabria@gmail.com>
2015-09-14 15:24:22 -0300

    Adds agenda files

--- init.el
+++ init.el
@@ -348,3 +348,15 @@
   (transpose-lines 1)
   (previous-line)
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org-notes/trustvox.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
