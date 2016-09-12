d11a9cf2655881a6f0945b0021d241799ec1bf32
Igor Fonte <igor.calabria@gmail.com>
2016-09-11 21:11:42 -0300

    Adds config for emacs 25

--- init.el
+++ init.el
@@ -61,6 +61,28 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (setq-default fill-column 100)
 
+(when (require 'xwidget nil 'noerror)
+  (define-key xwidget-webkit-mode-map [mouse-4] 'xwidget-webkit-scroll-down)
+  (define-key xwidget-webkit-mode-map [mouse-5] 'xwidget-webkit-scroll-up)
+  (define-key xwidget-webkit-mode-map (kbd "<up>") 'xwidget-webkit-scroll-down)
+  (define-key xwidget-webkit-mode-map (kbd "<down>") 'xwidget-webkit-scroll-up)
+  (define-key xwidget-webkit-mode-map (kbd "M-w") 'xwidget-webkit-copy-selection-as-kill)
+  (define-key xwidget-webkit-mode-map (kbd "C-c") 'xwidget-webkit-copy-selection-as-kill)
+
+  ;; adapt webkit according to window configuration chagne automatically
+  ;; without this hook, every time you change your window configuration,
+  ;; you must press 'a' to adapt webkit content to new window size
+  (add-hook 'window-configuration-change-hook (lambda ()
+                                                (when (equal major-mode 'xwidget-webkit-mode)
+                                                  (xwidget-webkit-adjust-size-dispatch))))
+
+  ;; make xwidget default browser
+  (setq browse-url-browser-function (lambda (url session)
+                                      (if (< (length (window-list)) 2)
+                                          (split-window-horizontally))
+                                      (other-window 1)
+                                      (xwidget-webkit-browse-url url))))
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((sh . t)
