ed21f14a177b9eae012551ea15a79abafdb9433e
igor.calabria <igor.calabria@ubee.in>
2019-05-02 14:53:41 -0300

    remove unstable melpa

--- init.el
+++ init.el
@@ -6,9 +6,6 @@
 (add-to-list
  'package-archives
  '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list
- 'package-archives
- '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; Bootstrap `use-package'
