091cc306313ec52cbc6146f94c3744b652e96ec7
igor.calabria <igor.calabria@ubee.in>
2020-02-28 14:14:22 -0300

    prefer spliting horizontally when opening buffers with helm

--- init.el
+++ init.el
@@ -248,7 +248,7 @@
   :config
   (progn
     (require 'helm-config)
-    (setq helm-split-window-in-side-p t)
+    (setq helm-window-prefer-horizontal-split t)
     (setq helm-recentf-fuzzy-match t)
     (setq helm-buffers-fuzzy-matching t)
     (setq helm-M-x-fuzzy-match t)
